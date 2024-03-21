b3fcf4f227c80377844746f02163b2994c0cefb2
vinnybrito <viny.rbrito15@hotmail.com>
2024-03-21 19:31:56 -0300

    Atualizando repositório

--- src/main/java/br/com/fiap/consultafacil/consulta/ConsultaController.java
+++ src/main/java/br/com/fiap/consultafacil/consulta/ConsultaController.java
@@ -1,5 +1,6 @@
 package br.com.fiap.consultafacil.consulta;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,12 @@ public class ConsultaController {
 
     @GetMapping
     public String index(Model model) {
+        model.addAttribute("consultas", List.of(
+            "Consulta 1",
+            "Consulta 2",
+            "Consulta 3",
+            "Teste Unitário"
+        ));
         return "consulta/index";
     }
     
